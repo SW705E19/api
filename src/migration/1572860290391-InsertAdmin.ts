9d6826cf4a927bc22e8f8f96c1daba14f627e2f4
Unknown <and8700and@gmail.com>
2019-11-07 09:42:26 +0100

    Migrations

--- src/migration/1572860290391-InsertAdmin.ts
+++ src/migration/1572860290391-InsertAdmin.ts
@@ -1,18 +1,8 @@
-import { MigrationInterface, QueryRunner, getRepository } from 'typeorm';
-import { User } from '../entity/user';
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
 
 export class InsertAdmin1572860290391 implements MigrationInterface {
 	public async up(queryRunner: QueryRunner): Promise<any> {
-		const user = new User();
-		user.username = 'admin';
-		user.password = 'admin';
-		user.email = 'admin@admin.com';
-		user.firstName = 'admin';
-		user.lastName = 'adminsen';
-		user.hashPassword();
-		user.roles = ['ADMIN'];
-		const userRepository = getRepository(User);
-		await userRepository.save(user);
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<any> {}
