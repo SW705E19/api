4623e13d6eeef027ceba5c9cfda85f0adb8d17f9
Daniel Moesgaard <moesgaarda@gmail.com>
2019-11-25 12:06:05 +0100

    Lint is now passing

--- src/tests/authController.spec.ts
+++ src/tests/authController.spec.ts
@@ -57,7 +57,6 @@ describe('auth controller login', function () {
 			}
 		});
 		const stubResult = sinon.stub(userService, 'getByEmail').throws('exception');
-		await AuthController.login(req, res);
 
 		await AuthController.login(req, res);
 		expect(res.statusCode).to.equal(400);
