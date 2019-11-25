34a24333a693fef62acde6eba5a0e01016af275e
Andreas Stenshøj <and8700and@gmail.com>
2019-11-25 09:53:36 +0100

    store email instead of username

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -28,10 +28,10 @@ class AuthController {
 		}
 
 		//Sign JWT, valid for 1 hour
-		const token = jwt.sign({ userId: user.id, username: user.username, roles: user.roles }, config.jwtSecret, {
+		const token = jwt.sign({ userId: user.id, username: user.email, roles: user.roles }, config.jwtSecret, {
 			expiresIn: '1h',
 		});
-    
+
 		//Send the jwt in the response
 		return res.send(token);
 	};
