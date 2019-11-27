3ad201b2c3e160d9e44b0d1d9c04945c02688a18
Frederik <frederikhtxq12@gmail.com>
2019-11-28 00:46:55 +0100

    fixed lint

--- src/tests/checkJwt.spec.ts
+++ src/tests/checkJwt.spec.ts
@@ -5,15 +5,15 @@ import { expect } from 'chai';
 import { checkJwt } from '../middlewares/checkJwt';
 
 describe('Check jwt middleware', function() {
-
-    afterEach(() => {
+	afterEach(() => {
 		sinon.restore();
-    });
-    it('returns status 401 if jwt isnt verified', async function() {
+	});
+	it('returns status 401 if jwt isnt verified', async function() {
 		const request = {
 			headers: {
-                authorization: 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoiYWRtaW5AYWRtaW4uZGsiLCJyb2xlcyI6WyJBRE1JTiIsIlRVVE9SIl0sImlhdCI6MTU3NDg5MzM0NSwiZXhwIjoxNTc0ODk2OTQ1fQ.J79OIIrwx2zhOH4fWZGS90b1RGNNMM3u5eHDr2Xqa0k'
-            },
+				authorization:
+					'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoiYWRtaW5AYWRtaW4uZGsiLCJyb2xlcyI6WyJBRE1JTiIsIlRVVE9SIl0sImlhdCI6MTU3NDg5MzM0NSwiZXhwIjoxNTc0ODk2OTQ1fQ.J79OIIrwx2zhOH4fWZGS90b1RGNNMM3u5eHDr2Xqa0k',
+			},
 		};
 		const req = mockReq(request);
 		const res = mockRes({
@@ -23,18 +23,19 @@ describe('Check jwt middleware', function() {
 			},
 		});
 
-		const mockGetBy = sinon.stub(jwt, 'verify').throws();
+		sinon.stub(jwt, 'verify').throws();
 		const next = sinon.spy();
 		const checkJWTCall = checkJwt;
 		await checkJWTCall(req, res, next);
 
 		expect(res.statusCode).to.equal(401);
-    });
-    it('returns error message if jwt is not verified', async function() {
+	});
+	it('returns error message if jwt is not verified', async function() {
 		const request = {
 			headers: {
-                authorization: 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoiYWRtaW5AYWRtaW4uZGsiLCJyb2xlcyI6WyJBRE1JTiIsIlRVVE9SIl0sImlhdCI6MTU3NDg5MzM0NSwiZXhwIjoxNTc0ODk2OTQ1fQ.J79OIIrwx2zhOH4fWZGS90b1RGNNMM3u5eHDr2Xqa0k'
-            },
+				authorization:
+					'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoiYWRtaW5AYWRtaW4uZGsiLCJyb2xlcyI6WyJBRE1JTiIsIlRVVE9SIl0sImlhdCI6MTU3NDg5MzM0NSwiZXhwIjoxNTc0ODk2OTQ1fQ.J79OIIrwx2zhOH4fWZGS90b1RGNNMM3u5eHDr2Xqa0k',
+			},
 		};
 		const req = mockReq(request);
 		const res = mockRes({
@@ -44,57 +45,59 @@ describe('Check jwt middleware', function() {
 			},
 		});
 
-		sinon.stub(jwt, 'verify').throws()
-        const next = sinon.spy();
+		sinon.stub(jwt, 'verify').throws();
+		const next = sinon.spy();
 		const checkJWTCall = checkJwt;
 		const checkJWTRes = await checkJWTCall(req, res, next);
 
 		expect(checkJWTRes).to.not.be.null;
 	});
-    it('calls next if jwt is valid', async function() {
+	it('calls next if jwt is valid', async function() {
 		const request = {
 			headers: {
-                authorization: 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoiYWRtaW5AYWRtaW4uZGsiLCJyb2xlcyI6WyJBRE1JTiIsIlRVVE9SIl0sImlhdCI6MTU3NDg5MzM0NSwiZXhwIjoxNTc0ODk2OTQ1fQ.J79OIIrwx2zhOH4fWZGS90b1RGNNMM3u5eHDr2Xqa0k'
-            },
+				authorization:
+					'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoiYWRtaW5AYWRtaW4uZGsiLCJyb2xlcyI6WyJBRE1JTiIsIlRVVE9SIl0sImlhdCI6MTU3NDg5MzM0NSwiZXhwIjoxNTc0ODk2OTQ1fQ.J79OIIrwx2zhOH4fWZGS90b1RGNNMM3u5eHDr2Xqa0k',
+			},
 		};
 		const req = mockReq(request);
 		const res = mockRes({
 			status: function(s: number) {
 				this.statusCode = s;
 				return this;
-            },
-            setHeader: function(s: string, x: string) {
-                this.s = x;
-                return this;
-            }
+			},
+			setHeader: function(s: string, x: string) {
+				this.s = x;
+				return this;
+			},
 		});
 
-        const next = sinon.spy();
-        sinon.stub(jwt, 'verify').resolves();
+		const next = sinon.spy();
+		sinon.stub(jwt, 'verify').resolves();
 		const checkJWTCall = checkJwt;
 		await checkJWTCall(req, res, next);
 
 		expect(next.calledOnce).to.be.true;
-    });
-    it('returns error message if jwt example is not verified', async function() {
+	});
+	it('returns error message if jwt example is not verified', async function() {
 		const request = {
 			headers: {
-                authorization: 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoiYWRtaW5AYWRtaW4uZGsiLCJyb2xlcyI6WyJBRE1JTiIsIlRVVE9SIl0sImlhdCI6MTU3NDg5MzM0NSwiZXhwIjoxNTc0ODk2OTQ1fQ.J79OIIrwx2zhOH4fWZGS90b1RGNNMM3u5eHDr2Xqa0k'
-            },
+				authorization:
+					'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoiYWRtaW5AYWRtaW4uZGsiLCJyb2xlcyI6WyJBRE1JTiIsIlRVVE9SIl0sImlhdCI6MTU3NDg5MzM0NSwiZXhwIjoxNTc0ODk2OTQ1fQ.J79OIIrwx2zhOH4fWZGS90b1RGNNMM3u5eHDr2Xqa0k',
+			},
 		};
 		const req = mockReq(request);
 		const res = mockRes({
 			status: function(s: number) {
 				this.statusCode = s;
 				return this;
-            },
-            setHeader: function(s: string, x: string) {
-                this.s = x;
-                return this;
-            }
+			},
+			setHeader: function(s: string, x: string) {
+				this.s = x;
+				return this;
+			},
 		});
 
-        const next = sinon.spy();
+		const next = sinon.spy();
 		const checkJWTCall = checkJwt;
 		await checkJWTCall(req, res, next);
 
