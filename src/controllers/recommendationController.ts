3aa0ee7167543d125d3bfc2d024890a81cef16a9
Andreas Stenshøj <and8700and@gmail.com>
2019-12-11 11:30:50 +0100

    comments and stuff

--- src/controllers/recommendationController.ts
+++ src/controllers/recommendationController.ts
@@ -13,11 +13,15 @@ class RecommendationController {
 	};
 
 	static calculateRecommendations = async (req: Request, res: Response): Promise<Response> => {
+		// Creates the matrix containing the recommendation values for each user/service combination.
 		const userServiceMatrix: number[][] = await Recommender.calculateRecommendations();
+		// Clear the db for recommendations so new ones can be inserted.
 		await RecommendationService.clearRecommendations();
 
 		const recommendations: Recommendation[] = [];
 
+		// Create the list of recommendation objects to be inserted into the db
+		// using the values from the matrix.
 		for (let i = 1; i < userServiceMatrix.length; i++) {
 			for (let j = 1; j < userServiceMatrix[0].length; j++) {
 				const recommendation: Recommendation = new Recommendation();
@@ -42,12 +46,14 @@ class RecommendationController {
 		return res.status(201).send(savedRecommendations);
 	};
 
+	// Get the currently logged in users recommendations
 	static getOwnRecommendations = async (req: Request, res: Response): Promise<Response> => {
 		const jwtPayload = res.locals.jwtPayload;
 		req.params.id = jwtPayload.userId;
 		return RecommendationController.getRecommendationsByUserId(req, res);
 	};
 
+	// Gets the given user's recommendations
 	static getRecommendationsByUserId = async (req: Request, res: Response): Promise<Response> => {
 		const userId: number = (req.params.id as unknown) as number;
 
@@ -61,19 +67,14 @@ class RecommendationController {
 		return res.status(200).send(recommendations);
 	};
 
+	// Gets the currently logged in users top recommendations
 	static getOwnTopRecommendations = async (req: Request, res: Response): Promise<Response> => {
 		const userId = res.locals.jwtPayload.userId;
-
-		let recommendations: Recommendation[];
-		try {
-			recommendations = await RecommendationService.getTopRecommendationsByUserId(userId);
-		} catch (error) {
-			recommendationsLogger.error(error);
-			return res.status(404).send('recommendations not found');
-		}
-		return res.status(200).send(recommendations);
+		req.params.id = userId;
+		return RecommendationController.getTopRecommendationsById(req, res);
 	};
 
+	// Gets the top recommendations for the give user.
 	static getTopRecommendationsById = async (req: Request, res: Response): Promise<Response> => {
 		const userId: number = (req.params.id as unknown) as number;
 
